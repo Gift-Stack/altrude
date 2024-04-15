b1e97007693d83c30297db18dc4f33f4e7ab2c75
GiFTED <gkenny896@gmail.com>
2024-04-15 22:26:20 +0100

    add walletconnect project id

--- wallet/config.ts
+++ wallet/config.ts
@@ -3,7 +3,7 @@ import { arbitrum, base } from "wagmi/chains";
 
 export const config = getDefaultConfig({
   appName: "My RainbowKit App",
-  projectId: "YOUR_PROJECT_ID",
+  projectId: "7726a18009e1545c6f26711c62c9d25c",
   chains: [arbitrum, base],
   ssr: true, // If your dApp uses server side rendering (SSR)
 });
